382f618a20a695a8bd97cd733c7f7d701972bb09
ruhtar <arthur.amorim10@gmail.com>
2026-02-03 21:32:13 -0300

    feat: wip - account service

--- src/KRTBank/KRTBank.Application/Services/AccountService.cs
+++ src/KRTBank/KRTBank.Application/Services/AccountService.cs
@@ -1,18 +1,46 @@
 using KRTBank.Application.DTOs;
 using KRTBank.Application.Interfaces;
+using KRTBank.Domain.Entities;
+using KRTBank.Domain.Interfaces;
 
 namespace KRTBank.Application.Services;
 
 public class AccountService : IAccountService
 {
-    public Task<Guid> CreateAsync(CreateAccountDto dto, CancellationToken cancellationToken = default)
+    private readonly IAccountRepository _repository;
+    
+    //TODO: FAZER CACHE E PUBLISHer
+
+    public AccountService(IAccountRepository repository)
     {
-        throw new NotImplementedException();
+        _repository = repository;
     }
 
-    public Task<AccountDto> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    public async Task<Guid> CreateAsync(CreateAccountDto dto, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var account = new Account(dto.HolderName, dto.Cpf);
+        
+        await _repository.AddAsync(account);
+        
+        return account.Id;
+    }
+
+
+    public async Task<AccountDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        // var cached = await _cache.GetAsync(id);
+        // if (cached != null) return cached;
+
+        var account = await _repository.GetByIdAsync(id);
+
+        if (account is null)
+        {
+            return null;
+        }
+        
+        // await _cache.SetAsync(dto);
+
+        return new AccountDto(account.Id, account.HolderName, account.Cpf, account.Status);
     }
 
     public Task UpdateAsync(Guid id, UpdateAccountDto dto, CancellationToken cancellationToken = default)
